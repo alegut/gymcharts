2099e290187a1920d1adad5fe6e91e6613d74cb5
guzenkov <wpandphp@gmail.com>
2019-09-27 10:37:19 +0300

    sidenav correction

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu'; 
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [],
   imports: [
-    MatButtonModule, 
+    MatButtonModule,
     MatIconModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule
   ],
   exports: [
-    MatButtonModule, 
+    MatButtonModule,
     MatIconModule,
     MatMenuModule,
-    MatToolbarModule,],
+    MatToolbarModule,
+    MatSidenavModule],
 })
 export class MaterialModule { }
